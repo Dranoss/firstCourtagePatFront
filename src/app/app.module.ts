81bba7e0930c2ee9105c6d7e9060da000d9e9832
Fabien <jouanneau.fab@gmail.com>
2020-06-01 21:54:09 +0200

    feat (routin-module): Creation routes et import modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './pages/nav-bar/nav-bar.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BrokerageComponent } from './pages/brokerage/brokerage.component';
+import { BusinessContributorComponent } from './pages/business-contributor/business-contributor.component';
+import { ConsultancyComponent } from './pages/consultancy/consultancy.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    HomeComponent,
+    BrokerageComponent,
+    BusinessContributorComponent,
+    ConsultancyComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
