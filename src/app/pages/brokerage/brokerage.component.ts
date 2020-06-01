ee043c91fce7f080a52aae2eaf0bb348a71f0be3
Fabien <jouanneau.fab@gmail.com>
2020-06-01 21:54:28 +0200

    feat(brokerage): creation page courtage

---
+++ src/app/pages/brokerage/brokerage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'apa-brokerage',
+  templateUrl: './brokerage.component.html',
+  styleUrls: ['./brokerage.component.scss']
+})
+export class BrokerageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
