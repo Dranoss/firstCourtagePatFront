2b0e07025012e4e709144d697be56e3791df4749
Fabien <jouanneau.fab@gmail.com>
2020-06-03 19:22:29 +0200

    feat(app-module): import module et composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { BusinessContributorComponent } from './pages/business-contributor/busin
 import { ConsultancyComponent } from './pages/consultancy/consultancy.component';
 import { LoginComponent } from './pages/login/login.component';
 import { YouTubePlayerModule } from '@angular/youtube-player';
-import { VideoComponent } from './video/video/video.component';
+import { VideoComponent } from './shared/components/video/video.component';
+import { EditableContentComponent } from './shared/components/editable-content/editable-content.component';
 
 @NgModule({
   declarations: [
@@ -25,12 +27,14 @@ import { VideoComponent } from './video/video/video.component';
     ConsultancyComponent,
     LoginComponent,
     VideoComponent,
+    EditableContentComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     YouTubePlayerModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
