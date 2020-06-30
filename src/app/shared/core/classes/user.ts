2b36e41e50842464b50554f3e727f2d2506a5a87
Rachid Guemili <rachidwilder33@gmail.com>
2020-06-30 20:43:41 +0200

    ok

--- src/app/shared/core/classes/user.ts
+++ src/app/shared/core/classes/user.ts
@@ -1,10 +1,11 @@
 import { Address } from './user-address';
 import { Rib } from './user-rib';
+import { Type } from './type';
 
 export class User {
-  public id?: number;
+  public id: number;
   public role: 'admin' | 'client';
-  public type: string;
+  public type: Type;
   public lastname: string;
   public firstname: string;
   public email: string;
@@ -16,34 +17,6 @@ export class User {
   public address: Address;
   public rib: Rib;
 
-  constructor(
-    role: 'admin' | 'client',
-    type: string,
-    lastname: string,
-    firstname: string,
-    email: string,
-    phoneNumber: string,
-    password: string,
-    societyName: string,
-    siretNumber: string,
-    sponsorshipCode: string,
-    address: Address,
-    rib: Rib,
-    id?: number,
-    ){
-
-    this.id = id;
-    this.role = role;
-    this.type = type;
-    this.lastname = lastname;
-    this.firstname = firstname;
-    this.email = email;
-    this.phoneNumber = phoneNumber;
-    this.password = password;
-    this.societyName = societyName;
-    this.siretNumber = siretNumber;
-    this.sponsorshipCode = sponsorshipCode;
-    this.address = address;
-    this.rib = rib;
+  constructor(){
   }
 }
