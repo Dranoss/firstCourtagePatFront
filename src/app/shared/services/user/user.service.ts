07ac11603bb23f81a1a22c87bd840a7306f24d01
Fabien <jouanneau.fab@gmail.com>
2020-07-01 16:30:54 +0200

    feat (user-service): change user

--- src/app/shared/services/user/user.service.ts
+++ src/app/shared/services/user/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable} from 'rxjs';
+import { Observable, ObservableInput} from 'rxjs';
 import { User } from '../../core/classes/user';
 
 @Injectable({
@@ -10,46 +10,52 @@ export class UserService {
   // A SUPPRIMER LORSQUE NOUS AURONS LE LIEN AVEC LE BACK
   users: User[] = [{
     role: 'client',
-    type: '',
-    firstname: 'Fabien',
-    lastname: 'Jouanneau',
+    userType: {id: 0, name: '', users: []},
+    firstName: 'Fabien',
+    lastName: 'Jouanneau',
     email: 'jouanneau.fab@gmail.com',
     phoneNumber: '0768195419',
     password: 'test',
-    societyName: '',
+    companyName: '',
     siretNumber: '',
     sponsorshipCode: '',
     id: 1,
-    address: {id: 1, addressNumber: 27, street: 'rue Saincric', zipCode: '33000', city: 'Bordeaux'},
-    rib: {id: 1, iban: '', bic: '', ownerFullname: '', bankName: ''}
+    userAddress: {id: 1, streetNumber: 27, streetName: 'rue Saincric', zipCode: '33000', cityName: 'Bordeaux'},
+    userRib: {id: 1, ibanNumber: '', bicCode: '', ownerName: '', bankName: ''}
   },
   ];
 
 // A DEFINIR POUR LIER LE BACK
-private USER_URL = '';
+private USER_URL = 'http://localhost:8080/users';
 
 constructor(private http: HttpClient) { }
 
-getUserById(id: number): User{
+// getUserById(id: number): User{
+
+
+getAllUsers(): Observable<User[]>{
+  return this.http.get<User[]>(this.USER_URL);
+}
+
 // A UTILISER LORSQUE L'ON AURA LE BACK
-// getUserById(id: number): Observable<User[]>{
-//   this.http.get<User>(`${this.USER_URL}/${id}`);
-// }
+getUserById(id: number): Observable<User>{
+  return this.http.get<User>(`${this.USER_URL}/${id}`);
+}
 
   // A SUPPRIMER LORSQUE L'ON AURA LA LIAISON BACK
-  const user = this.users.find(x => x.id === id);
-  return user;
-}
+  // const user = this.users.find(x => x.id === id);
+  // return user;
+  // }
 
 postUser(user: User): Observable<User>{
   return this.http.post<User>(this.USER_URL, user);
 }
 
 putUserById(user: User): Observable<User>{
-  return this.http.put<User>(this.USER_URL + `/${user.id}`, user);
+  return this.http.put<User>(`${this.USER_URL}/${user.id}`, user);
 }
 
-deleteUserById(user){
-  return this.http.delete<User>(this.USER_URL + `/${user.id}`, user);
+deleteUserById(id): Observable<void>{
+  return this.http.delete<void>(`${this.USER_URL}/${id}`);
 }
 }
