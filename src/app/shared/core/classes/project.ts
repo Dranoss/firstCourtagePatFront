f9f2f30b3011eadc15a2d9e02593abf6b8cdb254
Fabien <jouanneau.fab@gmail.com>
2020-07-06 10:12:05 +0200

    feat (project): add properties to the class

--- src/app/shared/core/classes/project.ts
+++ src/app/shared/core/classes/project.ts
@@ -1,13 +1,30 @@
+import { ProjectType } from './project-type';
+
 export class Project{
   public id?: number;
+  public amount: number;
   public name: string;
+  public projectType: ProjectType;
+  creationDate: Date;
+  closingDate: Date;
+  public documents?: Document;
 
   constructor(
     name: string,
+    amount: number,
+    projectType: ProjectType,
+    creationDate: Date,
+    closingDate: Date,
+    documents?: Document,
     id?: number
   ){
     this.id = id;
     this.name = name;
+    this.amount = amount;
+    this.projectType = projectType;
+    this.creationDate = creationDate;
+    this.closingDate = closingDate;
+    this.documents = documents;
   }
 
 }
