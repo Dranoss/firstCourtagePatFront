a6b3d15792bd98c115e21d41b04543fe62142f56
Fabien <jouanneau.fab@gmail.com>
2020-07-06 10:10:52 +0200

    feat (client-projects): create content

--- src/app/pages/client-projects/client-projects.component.ts
+++ src/app/pages/client-projects/client-projects.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { UserService } from '../../shared/services/user/user.service';
+import { User } from '../../shared/core/classes/user';
 
 @Component({
   selector: 'apa-client-projects',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-projects.component.scss']
 })
 export class ClientProjectsComponent implements OnInit {
-
-  constructor() { }
+  userId: number;
+  userType: string;
+  selectedUser: User;
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private userService: UserService,
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(paramMap => {
+      this.userId = +paramMap.get('id');
+      this.userType = paramMap.get('userType');
+      this.initializeUser();
+    });
   }
-
+  navigatoToProjectCreation(){
+    this.router.navigate(['/project-creation', this.userType, this.userId]);
+  }
+  initializeUser(){
+    this.userService.getUserById(this.userId).subscribe(data => {
+      this.selectedUser = data;
+      console.log(this.selectedUser);
+    });
+  }
 }
