cf4b07902910aff7c8d0835c22ff56a9e1a4e8c1
Fabien <jouanneau.fab@gmail.com>
2020-07-14 00:20:57 +0200

    feat (app-routing): add projectTypeListComponent, ProjectTypeCreationComponent, SettingsComponent, UserTypeListComponent, ProjectDetailComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,11 @@ import { ClientDetailsComponent } from './pages/client-details/client-details.co
 import { ClientProjectsComponent } from './pages/client-projects/client-projects.component';
 import { CreateClientComponent } from './pages/create-client/create-client.component';
 import { ProjectCreationComponent } from './pages/project-creation/project-creation.component';
+import { ProjectTypeListComponent } from './pages/project-type-list/project-type-list.component';
+import { ProjectTypeCreationComponent } from './pages/project-type-creation/project-type-creation.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { UserTypeListComponent } from './pages/user-type-list/user-type-list.component';
+import { ProjectDetailComponent } from './pages/project-detail/project-detail.component';
 
 
 const routes: Routes = [
@@ -29,11 +34,17 @@ const routes: Routes = [
   {path : 'recruitment', component : RecruitmentComponent},
   {path : 'partnership', component : PartnershipComponent},
   {path : 'corporate', component : CorporateComponent},
-  {path : 'clientView', component : ClientViewComponent},
-  {path : 'client-details/:userType/:id', component : ClientDetailsComponent},
-  {path : 'client-details/new-client', component : CreateClientComponent},
-  {path : 'client-projects/:userType/:id', component : ClientProjectsComponent},
-  {path: 'project-creation/:userType/:userId', component: ProjectCreationComponent},
+  {path : 'client-list', component : ClientViewComponent},
+  {path : 'client-details/:id', component : ClientDetailsComponent},
+  {path : 'new-client', component : CreateClientComponent},
+  {path : 'client-projects/:id', component : ClientProjectsComponent},
+  {path: 'project-creation/:userId', component: ProjectCreationComponent},
+  {path : 'settings', component : SettingsComponent},
+  {path : 'user-types', component : UserTypeListComponent},
+  {path : 'project-types', component : ProjectTypeListComponent},
+  {path : 'create-project-type', component : ProjectTypeCreationComponent},
+  {path : 'project-types/:id', component : ProjectTypeCreationComponent},
+  {path : 'project-details/:userId/:id', component : ProjectDetailComponent},
 ];
 
 @NgModule({
