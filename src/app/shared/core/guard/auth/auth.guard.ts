8fd35c5dfe51c80e02012dbf7e922bdd47cc98bc
Fabien <jouanneau.fab@gmail.com>
2020-07-17 00:01:12 +0200

    feat (auth.guard): creation

---
+++ src/app/shared/core/guard/auth/auth.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  token;
+
+  constructor(
+    private router: Router,
+  ){}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      this.token = localStorage.getItem('token');
+      if (this.token == null || this.token === ''){
+        this.router.navigateByUrl('/login');
+      } else {
+        return true;
+      }
+    }
+}
