9cbb185b8003d057cc18ad9a22c9678535a1b61b
Fabien <jouanneau.fab@gmail.com>
2020-07-16 21:06:03 +0200

    feat (login): add authService to login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../shared/services/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'apa-login',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  userModel = {email: '', password: ''};
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+  ) { }
 
   ngOnInit(): void {
   }
-
+  onSubmit(){
+    this.authService.login(this.userModel);
+  }
 }
