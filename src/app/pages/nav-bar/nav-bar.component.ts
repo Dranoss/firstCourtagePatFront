85449b8a571a83e706c88e6ccdc70fa3c13952eb
Fabien <jouanneau.fab@gmail.com>
2020-07-16 23:59:47 +0200

    feat (nav-bar): add deconnexion

--- src/app/pages/nav-bar/nav-bar.component.ts
+++ src/app/pages/nav-bar/nav-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../shared/services/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'apa-nav-bar',
@@ -6,10 +8,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent implements OnInit {
-
-  constructor() { }
+  displayDisconnectButton = false;
+  userId: number;
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.userId = +localStorage.getItem('userId');
+    setInterval(() => {
+      this.testConnection();
+    }, 1000);
   }
+  testConnection(){
+    this.userId = +localStorage.getItem('userId');
+    if (this.userId == null || this.userId === 0){
+      this.displayDisconnectButton = false;
+    } else {
+      this.displayDisconnectButton = true;
+    }
+    return this.displayDisconnectButton;
+  }
+  navigateDependingOnConnectionStatus(){
+    // EN COMMENTAIRE DANS L'ATTENTE DE SAVOIR QUELLE ROUTE UTILISER POUR LES ADMINS ET LES CLIENTS... (Fabien)
 
+    // const userRole = localStorage.getItem('userRole');
+    // if (userRole === 'admin'){
+    //   this.router.navigateByUrl('/client-list');
+    // } else if (userRole != null || userRole !== ''){
+    //   this.router.navigateByUrl('/client-projects/' + localStorage.getItem('userId'));
+    // } else {
+    //   this.router.navigateByUrl('/login');
+    //   this.displayDisconnectButton = false;
+    // }
+      this.router.navigateByUrl('/login');
+      this.displayDisconnectButton = false;
+  }
+  deconnectToken(){
+    this.authService.removeToken();
+    this.displayDisconnectButton = false;
+    this.router.navigateByUrl('/login');
+  }
 }
