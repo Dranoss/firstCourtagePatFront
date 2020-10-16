354c7940bdd6230a3aeccf3b4969bfc7bd978aac
Xavi <xaviregnier@gmail.com>
2020-10-16 19:15:55 +0200

    business contributor development

--- src/app/pages/Static pages/Principal/footer/footer.component.ts
+++ src/app/pages/Static pages/Principal/footer/footer.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  innerWidth: number;
   constructor() { }
 
   ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
   }
 
 }
